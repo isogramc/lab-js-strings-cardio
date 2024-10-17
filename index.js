533de031afd7c414b42c6e52fbd9a40d8a7d8f4f
theresajvrensburg <theresa@isogramconsulting.com>
2024-10-17 21:27:52 +0200

    Solved lab

--- index.js
+++ index.js
@@ -6,7 +6,11 @@
 const string1 = "My favorite dessert is jello";
 
 // Your code here...
-
+for (let i = 0; i<string1.length; i++){
+    if(string1[i]==="j"){
+        console.log(i);
+    }
+}
 
 
 
@@ -18,6 +22,7 @@ const string1 = "My favorite dessert is jello";
 const string2 = "ABCDEFGHJKLO";
 
 // Your code here...
+console.log(`${string2[2]}${string2[string2.length-1]}${string2[string2.length-1]}${string2[string2.length-2]}`)
 
 
 
@@ -30,9 +35,9 @@ const string2 = "ABCDEFGHJKLO";
 const string3 = "Na";
 
 // Your code here...
+let newString = string3.repeat(4);
 
-
-
+console.log(`${newString} Batman!`);
 
 /*******************************************
        Iteration 4 | Fruite Slice
@@ -40,8 +45,11 @@ const string3 = "Na";
 // Using the string method .slice(), access and print to the console the name of your favorite fruit from a given string
 
 const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
+console.log(fruit.slice(13, 18), fruit.slice(37, 47));
 
 // Your code here...
+let favouriteFruits = fruit.split(" ");
+console.log(favouriteFruits.slice(2,5));
 
 
 
@@ -55,14 +63,21 @@ const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pinea
 const funnyHeadline1 = "Breathing oxygen linked to staying alive";
 const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
-
 // Check the first headline
 // Your code here ...
-
+if(funnyHeadline1.includes("oxygen")===true){
+    console.log("The string includes oxygen");
+}else{
+    console.log("The string does not include oxygen");
+}
 
 // Check the second headline
 // Your code here ...
-
+if(funnyHeadline2.includes("oxygen")===true){
+    console.log("The string includes oxygen");
+}else{
+    console.log("The string does not include oxygen");
+}
 
 
 /*******************************************
@@ -75,7 +90,8 @@ const string4 = "zEAWrTC9EgtxmK9w1";
 
 // a) Print the string length
 // Your code here ...
-
+console.log(string4.length);
 
 // b) Print the last character in the string
 // Your code here ...
+console.log(string4[string4.length-1]);
